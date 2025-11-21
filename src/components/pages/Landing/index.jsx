be9fe3a479cb9f0869ffe99210b1343e96f95907
Dineury <DineuryLopez1@gmail.com>
2025-11-21 17:52:48 -0500

    already have images on the landing now  putting text on stack

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,10 +1,16 @@
-// import pieChart from '../../../assets/pie-chart.png';
-// import lineGraph from '../../../assets/line-graph.png';
-// import barGraph from '../../../assets/bar-graph.png';
-// import paperStack from '../../../assets/paper-stack.jpg';
+import pieChart from '../../../assets/pie-chart.png';
+ import lineGraph from '../../../assets/line-graph.png';
+import barGraph from '../../../assets/bar-graph.png';
+import paperStack from '../../../assets/paper-stack.jpg';
 import { useNavigate } from 'react-router-dom';
 import { useDownloadData } from '../../../hooks/useDownloadData.js';
 import {decodeBase64} from '../../../utils/decodeBase64.js';
+
+const imgObj = [
+  { src: barGraph, caption: "Search Grant Rates By Office", id: 1},
+  { src: pieChart, caption: "Search Grant Rates By Nationality", id:2 },
+  { src: lineGraph, caption: "Search Grant Rates Over Time", id: 3}
+]
 
 /**
  * TODO: Ticket 1:
@@ -32,8 +38,26 @@ export const LandingPage = () => {
 
   return (
     <div className='flex-c w-[100vw] secondary-c'>
-      Landing Page
-      <div>{'Type this into Canvas: ' + decodeBase64('VGltZTJDb2RlIQ==')}</div>
+
+      <div className="flex flex-row justify-center" > 
+       {
+        imgObj.map(img => {
+          return (
+            <div key={img.id} >
+            <img src={img.src} className='m-5 w-70 h-60 m' /> 
+        <p>{img.caption}</p>
+            </div>
+          )
+        })
+       }
+
+        
+      </div>
+      <br></br>
+      <div className='flex flex-row justify-around'>
+       <img src={paperStack} className='w-[45em] h-[27em] justify-self-center p-10 rounded-[50px] '/> 
+       <p className='content-center pr-[200px]'  >Hello</p>
+      </div>
     </div>
   );
 };
