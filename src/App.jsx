c62abd14725b3821c6ca2f0dde8d8d7290024167
Dineury <DineuryLopez1@gmail.com>
2025-12-02 19:51:42 -0500

    Implement Auth0 authentication and protect routes in the application

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,48 @@ import { NotFoundPage } from './components/pages/NotFound/index.jsx';
 import * as React from 'react';
 import Profile from './components/pages/Profile/index.jsx';
 import { pageWrapper } from './components/layout/PageWrapper.jsx';
+import { Auth0ProviderWithConfig } from './auth/auth0-provider-with-config';
+import { ProtectedRoute } from './components/pages/ProtectedRoute/ProtectedRoute.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: pageWrapper(<LandingPage />),
+    element: (
+      <Auth0ProviderWithConfig>
+        {pageWrapper(<LandingPage />)}
+      </Auth0ProviderWithConfig>
+    ),
     errorElement: <NotFoundPage />,
   },
   {
     path: '/graphs',
-    element: pageWrapper(<GraphsPage />),
+    element: (
+      <Auth0ProviderWithConfig>
+        {pageWrapper(<GraphsPage />)}
+      </Auth0ProviderWithConfig>
+    ),
     errorElement: <NotFoundPage />,
   },
   {
     path: '/profile',
-    element: pageWrapper(<Profile />),
+    element: (
+      
+      <Auth0ProviderWithConfig>
+        {pageWrapper(<ProtectedRoute>
+          <Profile />
+        </ProtectedRoute>)}
+      </Auth0ProviderWithConfig>
+    ),
     errorElement: <NotFoundPage />,
   },
+
+
 ]);
+
 
 export const App = () => {
   return (
+    
     <div className='font-serif w-[100vw] h-[100vh] m-0 flex-c justify-between align-centre text-center min-h-screen secondary-c'>
       <RouterProvider router={router} />
     </div>
--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -6,17 +6,23 @@
  * - Display the data
  * - Make this page a protected Route
  */
+import { useAuth0 } from "@auth0/auth0-react";
+
 const Profile = () => {
   // TODO: Replace these with functionality from Auth0
-  const isLoading = false;
-  const user = true;
+const { user, isAuthenticated, isLoading } = useAuth0();
+
 
   if (isLoading || !user) {
     return <div className='text-center p-4'>Loading...</div>;
   }
 
-  return (
-    <div>Profile Page</div>
+   return isAuthenticated && (
+    <div>
+      <img src={user.picture} alt={user.name} />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
   );
 };
 
